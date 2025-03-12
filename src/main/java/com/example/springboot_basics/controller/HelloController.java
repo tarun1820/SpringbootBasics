fb392cdcb05a3d1518be1634a950011f4cf8d7da
tarun1820 <edupugantitarunvenkatasai@gmail.com>
2025-03-12 12:39:01 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/example/springboot_basics/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.springboot_basics.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+    @GetMapping("/helloWorld")
+    public String sayHello(){
+        return "Hello world";
+    }
+}
